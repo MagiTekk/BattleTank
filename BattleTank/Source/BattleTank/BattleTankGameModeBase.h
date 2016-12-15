c9e0ec350d7c72351fcfebc1c9105f4d34910890
_nullbot <sonderblohm.dev@gmail.com>
2016-12-15 01:58:33 +0100

    [BT] Created the BattleTank project with Unreal and set up the git ignore file

---
+++ BattleTank/Source/BattleTank/BattleTankGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "BattleTankGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLETANK_API ABattleTankGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
