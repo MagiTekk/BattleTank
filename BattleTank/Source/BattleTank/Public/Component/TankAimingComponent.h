0a4a214a13368654ca2b7b5bc4daa46b600ef170
_nullbot <sonderblohm.dev@gmail.com>
2017-01-27 14:53:11 +0100

    [BT] Refactoring code - Fixing the crosshair's red color by default (Had to recompile the UI Widget)

--- BattleTank/Source/BattleTank/Public/Component/TankAimingComponent.h
+++ BattleTank/Source/BattleTank/Public/Component/TankAimingComponent.h
@@ -40,7 +40,7 @@ public:
 protected:
 
 	UPROPERTY(BlueprintReadOnly, Category = "Reloading")
-	EFiringStatus FiringStatus = EFiringStatus::Aiming;
+		EFiringStatus FiringStatus = EFiringStatus::Aiming;
 
 private:
 	UTankBarrel* Barrel = nullptr;
